210380739e5474190155c7eb1edb93e0fee96974
Dillon2332 <dillonduff1@frontier.com>
2020-01-15 11:42:50 -0500

    ..

--- VizardIntro.py
+++ VizardIntro.py
@@ -8,10 +8,20 @@ viz.setMultiSample(4)
 viz.go()
 
 viz.MainView.collision(viz.ON)
+# viz.MainView.setPosition([12, 15, -20])
+viz.MainView.setEuler([0, 0, 0])
 
 #Increase the Field of View
 viz.MainWindow.fov(60)
 
+viz.MainView.move([0,0,-8])
+
 piazza = viz.addChild('piazza.osgb')
-plant = viz.addChild('plant.osgb')
-plant.setPosition([4, 0, 6])
+
+
+piazza = viz.addChild('piazza.osgb')
+
+for x in [-3, -1, 1, 3]:
+    for z in [4, 2, 0, -2, -4]:
+        plant = viz.addChild('plant.osgb',cache=viz.CACHE_CLONE)
+        plant.setPosition([x,0,z])

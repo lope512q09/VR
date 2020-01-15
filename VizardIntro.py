efd417f53e24039f00c48951c00d3e96f7faf5a0
Dillon2332 <dillonduff1@frontier.com>
2020-01-15 11:22:44 -0500

    ..

---
+++ VizardIntro.py
@@ -0,0 +1,17 @@
+﻿import viz
+import vizshape
+
+
+#Enable full screen anti-aliasing (FSAA) to smooth edges
+viz.setMultiSample(4)
+
+viz.go()
+
+viz.MainView.collision(viz.ON)
+
+#Increase the Field of View
+viz.MainWindow.fov(60)
+
+piazza = viz.addChild('piazza.osgb')
+plant = viz.addChild('plant.osgb')
+plant.setPosition([4, 0, 6])
